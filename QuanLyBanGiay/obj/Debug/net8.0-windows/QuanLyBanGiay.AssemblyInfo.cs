12b7b17f9bc3280f9f6ba5216b3990806e667a95
maithiencan <maithiencan@gmail.com>
2025-04-30 02:07:12 +0700

    Go .vs va obj khoi Git và them .gitignore

--- QuanLyBanGiay/obj/Debug/net8.0-windows/QuanLyBanGiay.AssemblyInfo.cs
+++ QuanLyBanGiay/obj/Debug/net8.0-windows/QuanLyBanGiay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyBanGiay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f5d8adca1ee7bfedbfadea54e00df9474f958f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32525ecf0f48f2ee90debe8877172c9f25c9b579")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyBanGiay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyBanGiay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
