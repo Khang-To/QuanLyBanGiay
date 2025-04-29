dc55af2213763bdc0024e16714d1c54fb47ecb4c
To Van Khang <khangto1829@gmail.com>
2025-04-30 01:31:22 +0700

    hoan thien CSDL

---
+++ QuanLyBanGiay/Data/NhaCungCap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanGiay.Data
+{
+    internal class NhaCungCap
+    {
+        public int ID { get; set; }
+        public string TenNhaCungCap { get; set; } = null!;
+        public string? DienThoai { get; set; }
+        public string? DiaChi { get; set; }
+
+        public virtual ICollection<PhieuNhap> PhieuNhaps { get; set; } = new List<PhieuNhap>();
+    }
+}
