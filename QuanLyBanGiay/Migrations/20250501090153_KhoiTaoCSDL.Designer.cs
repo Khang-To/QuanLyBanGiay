1f5473f454b62b0d8f55c12852b3d781c4581fd3
maithiencan <maithiencan@gmail.com>
2025-05-01 16:03:25 +0700

    doi mat khau

--- QuanLyBanGiay/Migrations/20250501090153_KhoiTaoCSDL.Designer.cs
+++ QuanLyBanGiay/Migrations/20250501090153_KhoiTaoCSDL.Designer.cs
@@ -12,7 +12,7 @@ using QuanLyBanGiay.Data;
 namespace QuanLyBanGiay.Migrations
 {
     [DbContext(typeof(QLBGDbContext))]
-    [Migration("20250501082802_KhoiTaoCSDL")]
+    [Migration("20250501090153_KhoiTaoCSDL")]
     partial class KhoiTaoCSDL
     {
         /// <inheritdoc />
